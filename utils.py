6a1e63e215cf42078c80e6d063e8b5415e66ac6d
chen39137112 <641414768@qq.com>
2024-09-09 15:22:33 +0800

    限制访问速度

--- utils.py
+++ utils.py
@@ -101,6 +101,7 @@ def speed_limit(func):
     return decorated_func
 
 
+@speed_limit
 def url_get(url):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0"}
